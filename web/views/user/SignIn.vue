0b3d3e6d3149e7c8f5fbc5c1de3bff1deb3d542c
Antran <trannhatan2803@gmail.com>
2024-11-23 19:55:35 +0700

    update user signin

--- web/views/user/SignIn.vue
+++ web/views/user/SignIn.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 import TimeTable from "../../components/TimeTable.vue";
 import NavBar from "./NavBar.vue";
 import Carousel from "../../components/Carousel.vue";
@@ -7,15 +6,13 @@ import { defineComponent } from "vue";
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
 
-
 export default defineComponent({
     name: "SignIn",
-    data: function() {
+    data: function () {
         return {
-            username: '',
-            password: '',
+            username: "",
+            password: "",
             errorMessage: "",
-
         };
     },
     props: {
@@ -28,19 +25,20 @@ export default defineComponent({
                 const response = await fetch("/signin", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ username: this.username, password: this.password }),
+                    body: JSON.stringify({
+                        username: this.username,
+                        password: this.password,
+                    }),
                 });
 
                 if (!response.ok) throw new Error("Đăng nhập thất bại");
-                this.$router.push("/admin"); // Điều hướng đến trang yêu cầu đăng nhập
+                this.$router.push("/home"); // Điều hướng đến trang yêu cầu đăng nhập
             } catch (error: any) {
                 this.errorMessage = error.message;
             }
         },
     },
 });
-
-
 </script>
 
 <template>
@@ -50,23 +48,35 @@ export default defineComponent({
         <form @submit.prevent="login" method="POST">
             <div class="form-group">
                 <label for="username">Tên tài khoản</label>
-                <input v-model="username" type="text" class="form-control" id="username" name="username" required>
+                <input
+                    v-model="username"
+                    type="text"
+                    class="form-control"
+                    id="username"
+                    name="username"
+                    required
+                />
             </div>
             <div class="form-group">
                 <label for="password">Mật khẩu</label>
-                <input v-model="password" type="password" class="form-control" id="password" name="password" required>
+                <input
+                    v-model="password"
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    name="password"
+                    required
+                />
             </div>
             <p v-if="errorMessage">{{ errorMessage }}</p>
 
-            <button type="submit" class="btn btn-primary btn-block">Đăng ký</button>
+            <button type="submit" class="btn btn-primary btn-block">
+                Đăng ký
+            </button>
         </form>
     </div>
 
     <Footer />
-
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
